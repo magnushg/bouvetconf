b84bcf998da92a264bd9aae4ea8a3a3321cd2076
magnushg <magnus.h.green@gmail.com>
2014-03-08 18:29:30 +0100

    Decreased time interval for loading images

--- app/instaApp.js
+++ app/instaApp.js
@@ -12,23 +12,5 @@
     feed.run();
     setInterval(function () {
     	feed.run();
-    }, 300000);
+    }, 30000);
 }());
-
-(function () {
-    if (!String.prototype.format) {
-        var regexes = {};
-        String.prototype.format = function (parameters) {
-            for (var formatMessage = this, args = arguments, i = args.length; --i >= 0;)
-                formatMessage = formatMessage.replace(regexes[i] || (regexes[i] = RegExp("\\{" + (i) + "\\}", "gm")), args[i]);
-            return formatMessage;
-        };
-        if (!String.format) {
-            String.format = function (formatMessage, params) {
-                for (var args = arguments, i = args.length; --i;)
-                    formatMessage = formatMessage.replace(regexes[i - 1] || (regexes[i - 1] = RegExp("\\{" + (i - 1) + "\\}", "gm")), args[i]);
-                return formatMessage;
-            };
-        }
-    }
-})();
