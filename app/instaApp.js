3b8ee2e27e0c7026b70b35b1d1d6094306d3d5f6
magnushg <magnus.h.green@gmail.com>
2014-03-18 23:17:23 +0100

    Changed tag to bouvetone

--- app/instaApp.js
+++ app/instaApp.js
@@ -4,7 +4,7 @@
 
     var feed = new Instafeed({
         get: 'tagged',
-        tagName: 'bouvet',
+        tagName: 'bouvetone',
         clientId: 'c61ef3d655994bca9882f13621e54cfd',
         limit: 16,
         resolution: 'low_resolution'
