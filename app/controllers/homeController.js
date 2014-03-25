8e3f3a53894c9a650fafc5024f7d15d885d07c86
magnushg <magnus.h.green@gmail.com>
2014-03-25 20:11:42 +0100

    Tweaked design to be more bouvetish

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -10,7 +10,7 @@
         clientId: 'c61ef3d655994bca9882f13621e54cfd',
         limit: 16,
         resolution: 'low_resolution',
-        template: '<div class="imageContainer"><a href="{{link}}"><img src="{{image}}" /></a><div class="caption">{{caption}}</div></div>'
+        template: '<div class="imageContainer"><a href="{{link}}"><img src="{{image}}" /></a><div class="caption">{{user}}{{caption}}</div></div>'
     });
     feed.run();
     setInterval(function () {
