4a57581df8e15c8dc9660ea62f7c5edfa6292e96
unknown <magnus.green@R900N31X.bouvet.no>
2014-03-25 13:27:39 +0100

    Changed hashtag to festreisen

--- app/instaApp.js
+++ app/instaApp.js
@@ -4,7 +4,7 @@
 
     var feed = new Instafeed({
         get: 'tagged',
-        tagName: 'bouvetone',
+        tagName: 'festreisen',
         clientId: 'c61ef3d655994bca9882f13621e54cfd',
         limit: 16,
         resolution: 'low_resolution'
