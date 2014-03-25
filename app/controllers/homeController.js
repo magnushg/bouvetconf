325502e1cf741fb4e5aa464bb9a935ca1dfaf70b
magnushg <magnus.h.green@gmail.com>
2014-03-25 20:09:23 +0100

    Tweaked design to be more bouvetish

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,7 +1,21 @@
 (function(app) {
 
     var homeController = function($scope, $http) {       
-        $scope.message = "Hello world!"
+        $scope.message = "Hello world!";
+        $scope.tag =  "festreisen"
+
+        var feed = new Instafeed({
+        get: 'tagged',
+        tagName: $scope.tag,
+        clientId: 'c61ef3d655994bca9882f13621e54cfd',
+        limit: 16,
+        resolution: 'low_resolution',
+        template: '<div class="imageContainer"><a href="{{link}}"><img src="{{image}}" /></a><div class="caption">{{caption}}</div></div>'
+    });
+    feed.run();
+    setInterval(function () {
+    	feed.run();
+    }, 30000);
     };
 
 app.controller("homeController", ['$scope', '$http', homeController]);
--- app/instaApp.js
+++ app/instaApp.js
@@ -1,16 +1,5 @@
 (function () {
     
     angular.module("instaApp", []);
-
-    var feed = new Instafeed({
-        get: 'tagged',
-        tagName: 'festreisen',
-        clientId: 'c61ef3d655994bca9882f13621e54cfd',
-        limit: 16,
-        resolution: 'low_resolution'
-    });
-    feed.run();
-    setInterval(function () {
-    	feed.run();
-    }, 30000);
+    
 }());
